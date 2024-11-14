18d67240f374bc57f2e4ad5bbd3c9692119c49fe
Agung Supoyo <agungsupoyo@gmail.com>
2024-11-14 11:30:05 +0700

    latihan expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="auth-nav"  />
+        <Stack.Screen name="app-nav"  /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
---
+++ app/routing.js
@@ -0,0 +1,38 @@
+import { useEffect, memo } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen"; // Import SplashScreen
+import { Slot, Stack } from "expo-router";
+
+
+function RootLayout() {
+    const router = useRouter(); // Import useRouter from expo-router
+    const { uid, email_verified, apprenticeship_id } = useSelector(
+        (state) => state.user
+    );
+    
+    useEffect(() => {
+        // Prevent splash screen from auto-hiding
+        SplashScreen.preventAutoHideAsync();
+
+        if (!uid) {
+            router.replace("/auth-nav");
+        } else if (uid && !email_verified) {
+            router.replace("/auth-nav/verify-email");
+        } else if (!apprenticeship_id) {
+            router.replace("/apprentice-nav");
+        } else {
+            router.replace("/app-nav");
+        }
+
+        // Hide splash screen after authentication and routing check
+        SplashScreen.hideAsync();
+    }, [uid, email_verified, apprenticeship_id, router]);
+
+    return (
+        <Slot />
+    );
+    
+}
+
+export default RootLayout;
